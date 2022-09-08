3a8dfe1dabeb08c65509b21c183206ae818681ac
sidvud98 <sidvud98@gmail.com>
2022-09-08 20:02:11 +0530

    8th sept

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -9,11 +9,32 @@ class TaskController extends Controller{
         $tasks=Task::where('task_deleted','=','0');
         return response()->json($tasks->get());      
     }
-    public function showUserTasks(Request $request, $id)
+    public function showUserTasks($id, Request $request)
     {
-        $tasks=Task::where([['task_deleted','=','0'], ['assignee', $id]]);
-        return response()->json($tasks->get());
+        $sortingparam1 = $request->sort;
+        $sortingparam2 = $request->filterStatus;
+        if ($sortingparam1 != NULL) {
+            $tasks = Task::where([['task_deleted','=','0'], ['assignee', $id]])->orderBy($sortingparam1,'asc')->get();
+            return response()->json($tasks);
+        }
+        if ($sortingparam2 === 'pending') {
+            $tasks = Task::where('status', 'pending')->where([['task_deleted','=','0'], ['assignee', $id]])->get();
+            return response()->json($tasks);
+        } else if ($sortingparam2 === 'inprogress') {
+            $tasks = Task::where('status', 'inprogress')->where([['task_deleted','=','0'], ['assignee', $id]])->get();
+            return response()->json($tasks);
+        } else if ($sortingparam2 === 'overdue') {
+            $tasks = Task::where('status', 'overdue')->where([['task_deleted','=','0'], ['assignee', $id]])->get();
+            return response()->json($tasks);
+        } else if ($sortingparam2 === 'finished') {
+            $tasks = Task::where('status', 'finished')->where([['task_deleted','=','0'], ['assignee', $id]])->get();
+            return response()->json($tasks);
+        } else{
+            $tasks = Task::where([['task_deleted','=','0'], ['assignee', $id]])->get();
+            return response()->json($tasks);
+        }
     }
+
     public function createTask(Request $request){
         $requestData = $request->all();
         $this->validate($request, [
@@ -26,18 +47,18 @@ class TaskController extends Controller{
         // $task->update($requestData);
         return response()->json($task, 201);
     }
-    public function updateTask($id, Request $request){
+    public function updateTask($id, Request $request){    
         $requestData = $request->all();
         $task = Task::find($id);
         if(!Task::find($id)){
             return response()->json("Task doesn't exist", 400);
         }
-        dd($request);
-        $this->validate($request, [
-            'status' => 'required'
-        ]);
+        // dd($request->status);
+        // $this->validate($request, [
+        //     'status' => 'required'
+        // ]);
         $task->update($requestData);
-        $task->save();
+        // $task->save();
         return response()->json($task,200);
     }
     public function deleteTask($id){
--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -35,9 +35,10 @@ class Authenticate
      */
     public function handle($request, Closure $next, $guard = null)
     {
-        if ($this->auth->guard($guard)->guest()) {
-            return response('Unauthorized.', 401);
-        }
+        // dd($request);
+        // if ($this->auth->guard($guard)->guest()) {
+        //     return response('Unauthorized.', 401);
+        // }
 
         return $next($request);
     }
